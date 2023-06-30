91fd483602c613a32b5a89e0223b1cd0832112fc
valentyna-x <valentyna.khyzhniak@gmail.com>
2023-06-30 23:35:27 +0300

    master

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+console.log ('Hello Mad World of Front-End')
+
+
+const schoolname = 'BeetrootAcademy';
+console.log(schoolname);
+
+const coursename = 'Front-End';
+console.log(coursename);
+
+const country = 'Ukraine';
+console.log(country);
+
+let studentname = 'Valentyna Khyzhniak';
+console.log(studentname);
+
+let bestTeacherEver = 'Ivan Vasylenko';
+console.log(bestTeacherEver);
+
+
+
+console.log(schoolname+"\n"+coursename+"\n"+country+"\n"+studentname+"\n");
